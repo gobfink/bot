b69e9643201dfe02956e85cc8ca426d681d19606
Andy Fortman <gobfink@gmail.com>
2026-03-03 20:32:53 -0500

    Added some scripts for starting to sell archery supplies

---
+++ do_sell_archery.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import pyautogui
+from icecream import ic
+import time
+import random
+from tqdm import tqdm
+from utils import click, open_magic
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Reads the json file and clicks on the coordinates to perform bones to bananas')
+    parser.add_argument('--input', type=str, default='./alch.json', help='File with pixel coordinates')
+    parser.add_argument('--iterations', type=int, default=1, help='Number of iterations to complete')
+    return vars(parser.parse_args())
+
+args = parse_args()
+
+coords = None
+with open(args['input']) as f:
+    coords = json.load(f)
+
+assert coords
+
+for i in tqdm(range(args['iterations'])):
+    for item in coords.keys():
+        coord = coords[item]
+        ic(item, coord)
+        # Right click each item 
+        # Wait a tick
+        # Scroll to sell 5
+
+    ic(i)
+    ic('Selecting alch')
+    click(coords['alch'], .65)
+    ic('Alching item')
+    click(coords['item'], 3.5)
+    # How low can we go? a tick is 0.6 
+    # click(coords['item'], 1)
+
---
+++ sell_archery.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from icecream import ic
+import pyautogui
+import argparse
+import time
+
+from utils import write_json, get_one_left_click, click, open_magic
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Writes the json file to load into the doer')
+    parser.add_argument('--output', type=str, default='./alch.json', help='File to save the coordinates to')
+    return vars(parser.parse_args())
+
+
+args = parse_args()
+ic('Click mithril arrow')
+m_arrow = get_one_left_click()
+
+ic('Click adamant arrow')
+ad_arrow = get_one_left_click()
+
+ic('Click Oak longbow')
+o_longbow = get_one_left_click()
+
+ic('Click Willow shortbow')
+w_shortbow = get_one_left_click()
+
+ic('Click Willow longbow')
+w_longbow = get_one_left_click()
+
+ic('Click Maple shortbow')
+m_shortbow = get_one_left_click()
+
+ic('Click Maple longbow')
+m_longbow = get_one_left_click()
+
+coordinates = {
+    'mithril_arrow': m_arrow,
+    'adamant_arrow': ad_arrow,
+    'oak_longbow': o_longbow,
+    'willow_shortbow': w_shortbow,
+    'willow_longbow': w_longbow,
+    'maple_shortbow': m_shortbow,
+    'maple_longbow': m_longbow,
+}
+ic(coordinates)
+write_json(coordinates, args['output'])
+
+
