461b20fb385e3998a2af8d6641571c615b7acb96
Andy Fortman <gobfink@gmail.com>
2026-03-16 21:13:26 -0400

    Made it even tighter

--- samples/tune_yellow.py
+++ samples/tune_yellow.py
@@ -4,8 +4,8 @@ import numpy as np
 img = cv2.imread("image.png")
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-LOWER_YELLOW = np.array([22,170,170])
-UPPER_YELLOW = np.array([32,255,255])
+LOWER_YELLOW = np.array([24, 200, 200])
+UPPER_YELLOW = np.array([30, 255, 255])
 
 mask = cv2.inRange(hsv, LOWER_YELLOW, UPPER_YELLOW)
 
