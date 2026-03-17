f7d90747b630ec936346e9aa588d8618ad4e4756
Andy Fortman <gobfink@gmail.com>
2026-03-16 21:11:40 -0400

    added a tune yellow script

---
+++ samples/tune_yellow.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("image.png")
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+LOWER_YELLOW = np.array([22,170,170])
+UPPER_YELLOW = np.array([32,255,255])
+
+mask = cv2.inRange(hsv, LOWER_YELLOW, UPPER_YELLOW)
+
+cv2.imshow("mask", mask)
+cv2.waitKey(0)
